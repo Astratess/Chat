9ce023c1fa20bbbc706c9477cb3008040a42420d
skostov <st.kostow@gmail.com>
2023-06-25 18:43:32 +0300

    revert to Cookie

--- assets/js/userCheck.js
+++ assets/js/userCheck.js
@@ -1,7 +1,24 @@
+// export function getUser() {
+//     return localStorage.getItem("user");
+// }
+
+// export function setUser(name) {
+//     localStorage.setItem("user", name);
+// }
+
 export function getUser() {
-    return localStorage.getItem("user");
+    const cookie = document.cookie.match(new RegExp('(^| )' + 'user' + '=([^;]+)'));
+    
+    if (cookie != undefined) {
+        return cookie[2];
+    }
 }
 
 export function setUser(name) {
-    localStorage.setItem("user", name);
+    const d = new Date();
+	d.setTime(d.getTime() + (30*24*60*60*1000));
+
+	const expires = "expires=" + d.toUTCString();
+
+    document.cookie = `user=${name}` + expires;
 }
