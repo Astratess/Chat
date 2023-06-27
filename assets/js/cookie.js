dcf076f67bd661368481b9e34e4ee3544dfd5034
skostov <st.kostow@gmail.com>
2023-06-27 19:09:38 +0300

    Cookie update

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -3,14 +3,16 @@ export function getUser() {
     
     if (cookie != undefined) {
         return cookie[2];
+    } else if (localStorage.getItem('user') != undefined) {
+        return localStorage.getItem('user');
     }
 }
 
 export function setUser(name) {
-    // const d = new Date();
-	// d.setTime(d.getTime() + (30*24*60*60*1000));
+    const d = new Date();
+	d.setTime(d.getTime() + (30*24*60*60*1000));
 
-	// const expires = "expires=" + d.toUTCString();
+	const expires = "expires=" + d.toUTCString();
 
-    document.cookie = `user=${name}`;
+    document.cookie = `user=${name}; ${expires};path=/`;
 }
