97d40cf70ae36ee1a91fe3c6c53d91f8ae035833
skostov <st.kostow@gmail.com>
2023-06-27 22:06:53 +0300

    Service worker changes

--- sw.js
+++ sw.js
@@ -13,17 +13,97 @@ if ("serviceWorker" in navigator) {
     console.error("Service workers are not supported.");
   }
 
-  async function cacheThenNetwork(request) {
-    const cachedResponse = await caches.match(request);
-    if (cachedResponse) {
-      console.log("Found response in cache:", cachedResponse);
-      return cachedResponse;
-    }
-    console.log("Falling back to network");
-    return fetch(request);
-  }
-  
-  self.addEventListener("fetch", (event) => {
-    console.log(`Handling fetch event for ${event.request.url}`);
-    event.respondWith(cacheThenNetwork(event.request));
-  });
+// Core assets
+let coreAssets = [
+  '/assets/css/main.css',
+  '/assets/js/main.js',
+  '/assets/images/hexanect-logo.svg'
+];
+
+// On install, cache core assets
+self.addEventListener('install', function (event) {
+
+	// Cache core assets
+	event.waitUntil(caches.open('app').then(function (cache) {
+		for (let asset of coreAssets) {
+			cache.add(new Request(asset));
+		}
+		return cache;
+	}));
+
+});
+
+// Listen for request events
+self.addEventListener('fetch', function (event) {
+
+	// Get the request
+	let request = event.request;
+
+	// Bug fix
+	// https://stackoverflow.com/a/49719964
+	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+
+	// HTML files
+	// Network-first
+	if (request.headers.get('Accept').includes('text/html')) {
+		event.respondWith(
+			fetch(request).then(function (response) {
+
+				// Create a copy of the response and save it to the cache
+				let copy = response.clone();
+				event.waitUntil(caches.open('app').then(function (cache) {
+					return cache.put(request, copy);
+				}));
+
+				// Return the response
+				return response;
+
+			}).catch(function (error) {
+
+				// If there's no item in cache, respond with a fallback
+				return caches.match(request).then(function (response) {
+					return response || caches.match('/offline.html');
+				});
+
+			})
+		);
+	}
+
+	// CSS & JavaScript
+	// Offline-first
+	if (request.headers.get('Accept').includes('text/css') || request.headers.get('Accept').includes('text/javascript')) {
+		event.respondWith(
+			caches.match(request).then(function (response) {
+				return response || fetch(request).then(function (response) {
+
+					// Return the response
+					return response;
+
+				});
+			})
+		);
+		return;
+	}
+
+	// Images
+	// Offline-first
+	if (request.headers.get('Accept').includes('image')) {
+		event.respondWith(
+			caches.match(request).then(function (response) {
+				return response || fetch(request).then(function (response) {
+
+					// Save a copy of it in cache
+					let copy = response.clone();
+					event.waitUntil(caches.open('app').then(function (cache) {
+						return cache.put(request, copy);
+					}));
+
+					// Return the response
+					return response;
+
+				});
+			})
+		);
+	}
+
+});
